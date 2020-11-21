367f01e3e00d4826ed74f8e618a43626ac1f8b59
sravanichinna <sravanidane413.com>
2020-11-21 15:16:05 +0530

    Added: SingleCombination logic

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -12,3 +12,54 @@ then
 else
 	((tcount++))
 	echo -n "tail"
+if(( $a == 1 ))
+then
+	((hhcount++))
+if(( $b == 1 ))
+then
+	((hhhcount++))
+else
+	((hhtcount))
+fi
+else
+	((htcount++))
+if(( $b == 1 ))
+then
+	((hthcount++))
+else
+	((httcount++))
+fi
+fi
+else
+	((tcount++))
+	echo -n "tail"
+if (( $a == 1 ))
+then
+	((thcount++))
+if(( $b == 1 ))
+then
+	((thhcount++))
+else
+	((thtcount++))
+fi
+else
+	((ttcount++))
+if(( $b == 1 ))
+then
+	((tthcount++))
+else
+	((tttcount))
+fi
+fi
+fi
+done
+echo""
+echo "H percent:" `echo $hcount | awk'{print ($1/20)*100}'` "%"
+echo "T percent:" `echo $tcount | awk '{print ($1/20)*100}'` "%"
+echo -n "result:"
+if [[ $hcount -gt $tcount ]]
+then
+echo "HEAD WON"
+else
+echo "TAIL WON"
+fi
