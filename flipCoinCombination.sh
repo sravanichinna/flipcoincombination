4965c3e96f4b206e6c06d41b6a73ee2a0da5af9f
sravanichinna <sravanidane413.com>
2020-11-21 15:28:23 +0530

    Added: TripletCombination logic

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -59,9 +59,9 @@ echo "T percent:" `echo $tcount | awk '{print ($1/20)*100}'` "%"
 echo -n "result:"
 if [[ $hcount -gt $tcount ]]
 then
-echo "HEAD WON"
+	echo "HEAD WON"
 else
-echo "TAIL WON"
+	echo "TAIL WON"
 fi
 echo "HH percent:" `echo $hhcount | awk'{print ($1/20)*100}'` "%"
 echo "HT percent:" `echo $htount | awk'{print ($1/20)*100}'` "%"
@@ -70,13 +70,49 @@ echo "TT percent:" `echo $ttcount | awk'{print ($1/20)*100}'` "%"
 echo "result:"
 if [[ $hhcount > $htcount && $hhcount > $thcount && $hhcount > $ttcount ]]
 then
-echo "HEAD HEAD WON"
+	echo "HEAD HEAD WON"
 elif [[ $htcount > $hhcount && $htcount >$thcount && $htcount > $ttcount ]]
 then
-echo "HEAD TAIL WON"
+	echo "HEAD TAIL WON"
 elif [[ $thcount > $hhcount && $thcount >$htcount && $thcount > $ttcount ]]
 then 
-echo"TAIL HEAD WON"
+	echo"TAIL HEAD WON"
 else
-echo "TAIL TAIL WON"
+	echo "TAIL TAIL WON"
+fi
+echo "HHH percent:" `echo $hhhcount | awk '{print ($1/20)*100'` "%"
+echo "HHT percent:" `echo $hhtcount | awk '{print ($1/20)*100'` "%"
+echo "HTH percent:" `echo $hthcount | awk '{print ($1/20)*100'` "%"
+echo "HTT percent:" `echo $httcount | awk '{print ($1/20)*100'` "%"
+echo "THH percent:" `echo $thhcount | awk '{print ($1/20)*100'` "%"
+echo "THT percent:" `echo $thtcount | awk '{print ($1/20)*100'` "%"
+echo "TTH percent:" `echo $tthcount | awk '{print ($1/20)*100'` "%"
+echo "TTT percent:" `echo $tttcount | awk '{print ($1/20)*100'` "%"
+
+
+echo "result:"
+if [[ $hhhcount > $hhtcount && $hhhcount > $hthcount && $hhhcount > $httcount && $hhhcount > $thhcount && $hhhcount > $thtcount && $hhhcount > $tthcount && $hhhcount > $tttcount ]]
+then
+	echo "HEAD HEAD HEAD WON"
+elif [[ $hhtcount > $hhhcount && $hhtcount > $hthcount && $hhtcount > $httcount && $hhtcount >$thhcount && $hhtcount > $thtcount && $hhtcount > $tthcount && $hhtcount > $tttcount ]]
+then
+	echo "HEAD HEAD TAIL WON"
+elif [[ $hthcount > $hhtcount && $hthcount > $hhhcount && $hthcount > $httcount && $hthcount > $thhcount && $hthcount > $thtcount && $hthcount >$tthcount && $hthcount > $tttcount ]]
+then
+	echo "HEAD TAIL HEAD WON"
+elif [[ $httcount > $hhtcount && $httcount > $hthcount && $httcount > $hhhcount && $httcount > $thhcount && $httcount > $thtcount && $httcount > $tthcount && $httcount > $tttcount ]]
+then
+	echo "HEAD TAIL TAIL WON"
+elif [[ $thhcount > $hhtcount && $thhcount > $hthcount && $thhcount >$httcount && $thhcount > $hhhcount && $thhcount > $thtcount && $thhcount > $tthcount > $tttcount ]]
+then
+	echo "TAIL HEAD HEAD WON"
+elif [[ $thtcount > $hhtcount && $thtcount > $hthcount && $thtcount > $httcount && $thtcount >  $thhcount && $thtcount > $hhtcount && $thtcount > $tthcount && $thtcount > $tttcount ]]
+then
+	echo "TAIL HEAD TAIL WON"
+elif [[ $tthcount > $hhtcount && $tthcount > $hthcount && $tthcount > $httcount && $tthcount > $thhcount && $tthcount > $thtcount && $tthcount > $hhhcount && $tthcount > $tttcount ]]
+then
+	echo "TAIL TAIL HEAD WON"
+elif [[ $tttcount > $hhtcount && $tttcount > $hthcount && $tttcount > $httcount && $tttcount > $thhcount && $tttcount > $thtcount && $tttcount > $tthcount && $tttcount > $hhhcount ]]
+then
+	echo "TAIL TAIL TAIL WON"
 fi
