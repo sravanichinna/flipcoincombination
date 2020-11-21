50f463ce3049c949bbb4bd8b45785249f793d4ed
sravanichinna <sravanidane413.com>
2020-11-21 15:09:48 +0530

    Added: FlipCoin logic

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "welcome to flip coin combination"
+for((i=0;i<20;i++))
+do
+b=$a
+a=$coin
+coin=$(($RANDOM%2 ))
+if (( $coin == 1 ))
+then
+	((hcount++))
+	echo -n "head"
+else
+	((tcount++))
+	echo -n "tail"
